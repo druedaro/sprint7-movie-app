b47dd2aee3b0808a70d56e7c3f8d43873c99bd44
David <ruedarosasdavid@gmail.com>
2025-10-17 13:57:32 +0200

    feat: create welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './auth/AuthContext';
 import { PATHS } from './routes/paths';
+import WelcomePage from './pages/WelcomePage';
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path={PATHS.HOME} element={<div>Welcome Page (Coming soon)</div>} />
+          <Route path={PATHS.HOME} element={<WelcomePage />} />
           <Route path={PATHS.AUTH} element={<div>Auth Page (Coming soon)</div>} />
           <Route path={PATHS.MOVIES} element={<div>Movies Page (Coming soon)</div>} />
         </Routes>
